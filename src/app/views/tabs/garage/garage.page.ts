522ea3a6331279db0b7ae7b39f8e9492f78cf120
rsoares21 <ricardo.soares@gmail.com>
2022-11-28 02:04:35 -0300

    coding

--- src/app/views/tabs/garage/garage.page.ts
+++ src/app/views/tabs/garage/garage.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 
+
 @Component({
   selector: 'app-garage',
   templateUrl: './garage.page.html',
@@ -10,25 +11,24 @@ export class GaragePage implements OnInit {
 
   playerData;
   playerAccount;
-  constructor(private router: Router) { 
+  constructor(private router: Router) {
 
   }
 
   ngOnInit() {
-    
-    
+
+
 
     let isLoggedIn = sessionStorage.getItem('isLoggedIn') != null
     if (!isLoggedIn) this.router.navigate(['/login'])
 
-    console.log(JSON.stringify('isLoggedIn:'+isLoggedIn))
+    console.log(JSON.stringify('isLoggedIn:' + isLoggedIn))
 
-    this.playerData = JSON.parse(sessionStorage.getItem('pdata')); 
+    this.playerData = JSON.parse(sessionStorage.getItem('pdata'));
     this.playerAccount = JSON.stringify(this.playerData.account.actor)
-    this.playerAccount = this.playerAccount.replace("\"","").replace("\"","")
-    
-    console.log('player_data:'+JSON.stringify(this.playerData))
-    
+    this.playerAccount = this.playerAccount.replace("\"", "").replace("\"", "")
+
+    console.log('player_data:' + JSON.stringify(this.playerData))
 
   }
 
